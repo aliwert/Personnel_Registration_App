21d43213eed50cc18ced7b2518e984639185c269
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-07 21:58:45 +0300

    added sum salary and avg salary

--- Personel_Kayit1_App/FormStatistics.cs
+++ Personel_Kayit1_App/FormStatistics.cs
@@ -51,6 +51,40 @@ namespace Personel_Kayit1_App
             }
             baglanti.Close();
 
+
+            // number of city
+            baglanti.Open();
+            SqlCommand komut4 = new SqlCommand("Select Count(distinct(PerSehir)) From Tbl_Personel", baglanti);
+            SqlDataReader dr4 = komut4.ExecuteReader();
+            while (dr4.Read())
+            {
+                LblSehirSayisi.Text = dr4[0].ToString();
+            }
+            baglanti.Close();
+
+
+            // sum salary
+
+            baglanti.Open();
+            SqlCommand komut5 = new SqlCommand("Select Sum(PerMaas) From Tbl_Personel", baglanti);
+            SqlDataReader dr5 = komut5.ExecuteReader();
+            while(dr5.Read())
+            {
+                LblToplamMaas.Text = dr5[0].ToString();
+            }
+            baglanti.Close();
+
+            // avg salary
+
+            baglanti.Open();
+            SqlCommand komut6 = new SqlCommand("Select Avg(PerMaas) From Tbl_Personel", baglanti);
+            SqlDataReader d6 = komut6.ExecuteReader();
+            while(d6.Read())
+            {
+                LblOrtalamaMaas.Text = d6[0].ToString();
+            }
+            baglanti.Close();
+
         }
     }
 }
